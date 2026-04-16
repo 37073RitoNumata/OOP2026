f6205d0bc020cbd8516fa1147d9774e320da86fa
infosys <infosys@C4230>
2026-04-16 10:57:29 +0900

    プロジェクト ファイルを追加します。

---
+++ chapter00/Sample0413/Sample0413/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Sample0413
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("表示回数：");
+
+            string? inputNum = Console.ReadLine();
+
+            int count = int.Parse(inputNum); //整数へ変換
+
+            int i = 0;
+
+            while (i < count)
+            {
+                if ((i + 1) % 2 == 0)
+                {
+                    Console.Write((i + 1) + ":");
+                    Console.WriteLine("Hello, World!");
+                }
+
+                i++;
+
+            }
+        }
+    }
+}
---
+++ chapter00/Sample0415/Sample0415/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Sample0415
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            //入力
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "] = ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            //集計
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+            //}
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("array[" + i + "]:");
+                astOut(array[i]);
+            }
+
+            Console.WriteLine("合計:" + array.Sum(n => n % 2));
+
+        }
+            //出力
+            static void astOut(int num)
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+    }
+}
