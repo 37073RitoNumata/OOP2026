0aab7c958c7c33304cb13c36787dfa6d14edcdb1
里駿 沼田 <oibc37073@ojs.ac.jp>
2026-05-13 10:23:21 +0900

    曲名、アーティスト名、演奏時間の入力

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var songs = new List<Song>();
+
+            while (true) {
+                Console.WriteLine("曲名：");
+                string? title = Console.ReadLine();
+
+                if (title == "end") {
+                    break;
+                } else {
+
+                    Console.WriteLine("アーティスト名：");
+                    string? artistName = Console.ReadLine();
+
+                    Console.WriteLine("演奏時間");
+                    int length = int.Parse(Console.ReadLine());
+
+                    Song song = new Song(title, artistName, length);
+
+                    songs.Add(song);
+                }
+            }
+
+            PrintSongs(songs);
+
+
+            //2.1.3
+            //var songs = new Song[] {
+            // new Song("Let it be", "The Beatles", 243),
+            // new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+            // new Song("Close To You", "Carpenters", 276),
+            // new Song("Honesty", "Billy Joel", 231),
+            // new Song("I Will Always Love You", "Whitney Houston", 273),
+            //};
+        }
+        //2.1.4
+        private static void PrintSongs(List<Song> songs) {
+            foreach (var song in songs) {
+                Console.WriteLine($"{song.Title}, {song.ArtistName},{song.Length / 60}:{song.Length % 60:00}");
+            }
+        }
+    }
+}
