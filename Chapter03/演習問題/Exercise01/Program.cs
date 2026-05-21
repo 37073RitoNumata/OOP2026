07b1195f18aac6886360993e3cd2a0bfa9a43c46
里駿 沼田 <oibc37073@ojs.ac.jp>
2026-05-21 10:02:04 +0900

    問題3.1.1完成

---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers) {
+            var exists = numbers.Exists(i => i % 8 == 0 || i % 9 == 0);
+            if (exists) {
+                Console.WriteLine("存在しています");
+            } else {
+                Console.WriteLine("存在していません");
+            }
+        }
+
+        private static void Exercise2(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.2完成」
+        }
+
+        private static void Exercise3(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.3完成」
+        }
+
+        private static void Exercise4(List<int> numbers) {
+            //できたらGitのコメント「問題3.1.4完成」
+        }
+    }
+}
