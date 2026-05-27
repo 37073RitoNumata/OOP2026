8d934aa6688261ffbfe77ae63f715f4403667fc1
里駿 沼田 <oibc37073@ojs.ac.jp>
2026-05-27 11:04:07 +0900

    4.1.1完成

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Linq;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+                "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            Console.WriteLine(" --- 4.1.1 ---");
+
+            //foreach
+            Console.WriteLine("\nforeach");
+            var f0re = langs.Where(s => s.Contains('S'));
+            foreach (var s in f0re) {
+                Console.WriteLine(s);
+            }
+
+
+
+            //for
+            Console.WriteLine("\nfor");
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S')) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+
+
+
+
+
+            //while  
+            Console.WriteLine("\nwhile");
+            int count = 0;
+            while (count < langs.Count) {
+                if (langs[count].Contains('S')) {
+                    Console.WriteLine(langs[count]);
+                }
+                count++;
+            }
+        }
+
+        private static void Exercise2(List<string> langs) {
+            Console.WriteLine("\n --- 4.1.2 ---");
+            //LINQ  g p    iWhere j
+
+
+        }
+
+        private static void Exercise3(List<string> langs) {
+            Console.WriteLine("\n --- 4.1.3 ---");
+
+        }
+    }
+}
