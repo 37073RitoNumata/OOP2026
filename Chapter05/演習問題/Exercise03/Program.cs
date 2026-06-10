943303bb333c7665b3a54828173efcf42635a737
里駿 沼田 <oibc37073@ojs.ac.jp>
2026-06-10 10:39:01 +0900

    6.2完成

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercise01;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var ym = new YearMonth[] {
+                new YearMonth(2024, 12),
+                new YearMonth(2024, 12),
+            };
+
+            if (ym[0] == ym[1]) {
+                Console.WriteLine("等しい");
+            }
+        }
+        
+
+    }
+}
---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            bool Bool = int.TryParse(Console.ReadLine(), out int num);
+
+            var str = num.ToString("#,0");
+
+            Console.WriteLine(str);
+        }
+    }
+}
