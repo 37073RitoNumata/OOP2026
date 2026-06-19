e464bbac55f1a017da81b25a10034a728e91b8fd
里駿 沼田 <oibc37073@ojs.ac.jp>
2026-06-19 10:43:30 +0900

    7.1.4

---
+++ Chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+            #region
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+            #endregion
+        }
+
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            foreach (var i in numbers.Reverse().Take(2)) {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void Exercise3(int[] numbers) {
+            for (int i = 0; i < numbers.Length; i++) {
+                Console.WriteLine($"{numbers[i]:000}");
+            }
+        }
+
+        private static void Exercise4(int[] numbers) {
+            foreach (var i in numbers.OrderBy(x => x).Take(3)) {
+                Console.WriteLine(i);
+            }
+        }
+
+        private static void Exercise5(int[] numbers) {
+        }
+    }
+}
