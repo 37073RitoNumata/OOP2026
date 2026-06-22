7dfb134981d31ac445ae2d7197c9f6147882f297
里駿 沼田 <oibc37073@ojs.ac.jp>
2026-06-22 11:36:52 +0900

    設問１_Studentクラス完成

---
+++ TestProject/Test01_01/ScoreCounter.cs
@@ -0,0 +1,44 @@
+﻿namespace Test01_01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+            _score = ReadScore(filePath);
+        }
+
+        //メソッドの概要：
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            var sales = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines) {
+                var items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+
+                sales.Add(student);
+            }
+
+
+
+
+
+            return sales;
+        }
+
+        //メソッドの概要：
+        public IDictionary<string, int> GetPerStudentScore() {
+            var dict = new Dictionary<string, int>();
+
+
+
+
+
+
+            return dict;
+        }
+    }
+}
